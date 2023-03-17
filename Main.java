bddd25853e2c3283c74649b04bd623248ecaa923
Ujjal Khadka <khadkauzal@gmail.com>
2023-03-17 21:13:53 +0545

    completed-step-2/5

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input string:");
+        String input = scanner.nextLine();
+
+        System.out.println("The result:");
+        for(int i = 0; i < input.length(); i++){
+            System.out.println(input.charAt(i) + " = " + String.format("%07d", Integer.parseInt(Integer.toBinaryString(input.charAt(i)))));
+        }
+    }
+}
